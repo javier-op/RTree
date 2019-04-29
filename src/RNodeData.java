db8a84f509d8c17121aa34f225a4da393107fd11
javier-op <javiermoralesr95@gmail.com>
2019-04-29 15:54:40 -0400

    Ahora lo hice bien.

--- src/RNodeData.java
+++ src/RNodeData.java
@@ -1,11 +1,2 @@
-import java.io.Serializable;
-import java.util.ArrayList;
-
-public class RNodeData implements Serializable {
-    public int size;
-    public long id;
-    public char type;
-    public long parent_id;
-    public ArrayList<Long> children_ids;
-    public ArrayList<Rectangle> children_rectangles;
+public class RNodeDate {
 }
