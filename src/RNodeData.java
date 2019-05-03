09a6a18f6b5bcb06e5e415b115d65c70f93d7bb8
javier-op <javiermoralesr95@gmail.com>
2019-05-02 20:16:49 -0400

    Agrego updateMBR y esqueleto para splits.

--- src/RNodeData.java
+++ src/RNodeData.java
@@ -3,9 +3,10 @@ import java.util.ArrayList;
 
 public class RNodeData implements Serializable {
     public int size;
-    public long id;
     public char type;
-    public long parent_id;
+    public Long id;
+    public Long parent_id;
+    public Integer index_in_parent;
     public ArrayList<Long> children_ids;
     public ArrayList<Rectangle> children_rectangles;
 }
--- src/RTree.java
+++ src/RTree.java
@@ -7,15 +7,16 @@ public class RTree {
     private long next_id;
     private String dir;
     private int max_size;
+    private char mode;
     private ArrayList<Rectangle> search_result;
-    // private ISplitter splitter;
 
-    public RTree(String dir_name, int max_size) {
-        this.dir = dir_name;
+    public RTree(String dir_name, int size, char split_mode) {
+        assert split_mode == 0 || split_mode == 1;
+        dir = dir_name;
         root_id = 0L;
         next_id = 1L;
-        this.max_size = max_size;
-        //this.splitter = splitter;
+        max_size = size;
+        mode = split_mode;
         node = new RNodeData();
         node.id = root_id;
         node.type = 'l';
@@ -44,15 +45,17 @@ public class RTree {
         insert(value, root_id);
     }
 
-    private void insert(Rectangle value, long node_id) {
+    private void insert(Rectangle value, Long node_id) {
         loadNode(node_id);
         if(node.type == 'l') {
             node.children_rectangles.add(value);
             node.size++;
-            saveNode();
-            // if(node.size > max_size) {
-            //    splitter.split(node_id);
-            //}
+            if(node.size > max_size) {
+                split();
+            } else {
+                saveNode(node);
+                updateMBR(node.parent_id, node.index_in_parent, value);
+            }
         } else if (node.type == 'n') {
             int best_node_index = 0;
             double best_node_area = Double.MAX_VALUE;
@@ -70,6 +73,36 @@ public class RTree {
         }
     }
 
+    private void updateMBR(Long node_id, int rectangle_index, Rectangle extension) {
+        if(node_id != null) {
+            loadNode(node_id);
+            Rectangle new_rectangle = extend(node.children_rectangles.get(rectangle_index), extension);
+            node.children_rectangles.set(rectangle_index, new_rectangle);
+            saveNode(node);
+            updateMBR(node.parent_id, node.index_in_parent, extension);
+        }
+    }
+
+    private void split() {
+        RNodeData[] new_children;
+        if(mode == 0) {
+            new_children = linearSplit();
+        } else {
+            new_children = quadraticSplit();
+        }
+        loadNode(node.parent_id);
+    }
+
+    private RNodeData[] linearSplit() {
+        RNodeData[] output = new RNodeData[]{};
+        return output;
+    }
+
+    private RNodeData[] quadraticSplit() {
+        RNodeData[] output = new RNodeData[]{};
+        return output;
+    }
+
     public ArrayList<Rectangle> search(double x0, double x1, double y0, double y1) {
         Rectangle value = new Rectangle(x0, x1, y0, y1);
         search_result = new ArrayList<Rectangle>();
@@ -77,7 +110,7 @@ public class RTree {
         return search_result;
     }
 
-    private void search(Rectangle value, long node_id) {
+    private void search(Rectangle value, Long node_id) {
         loadNode(node_id);
         ArrayList<Long> valid_nodes = new ArrayList<Long>();
         if(node.type == 'l') {
@@ -98,7 +131,7 @@ public class RTree {
         }
     }
 
-    private void loadNode(long node_id) {
+    private void loadNode(Long node_id) {
         String fileName = "n" + node_id + ".bin";
         try {
             FileInputStream fis = new FileInputStream(dir + "/" + fileName);
@@ -111,12 +144,12 @@ public class RTree {
         }
     }
 
-    private void saveNode() {
-        String fileName = "n" + node.id + ".bin";
+    private void saveNode(RNodeData a_node) {
+        String fileName = "n" + a_node.id + ".bin";
         try {
             FileOutputStream fos = new FileOutputStream(dir + "/" + fileName);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(node);
+            oos.writeObject(a_node);
             oos.close();
             fos.close();
         } catch (IOException e) {
@@ -127,10 +160,6 @@ public class RTree {
     private boolean intersect(Rectangle r0, Rectangle r1) {
         return r0.x0 <= r1.x1 && r0.x1 >= r1.x0 &&
                 r0.y1 >= r1.y0 && r0.y0 <= r1.y1;
-    }
-
-    private double area(Rectangle r) {
-        return (r.x1 - r.x0) * (r.y1 - r.y0);
     }
 
     private double areaDifference(Rectangle r0, Rectangle r1) {
