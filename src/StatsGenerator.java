8d26a70f7f5eff746b8d484a3a5f5d37a43d8f25
javier-op <javiermoralesr9@gmail.com>
2019-05-06 20:16:22 -0400

    apurate ctm 2

--- src/StatsGenerator.java
+++ src/StatsGenerator.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import org.apache.commons.io.FileUtils;
 
-public class LinearTest {
+public class StatsGenerator {
 
     public static Rectangle rectangleGenerator(){
         double leftLimit = 0D;
